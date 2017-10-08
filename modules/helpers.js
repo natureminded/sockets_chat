bd60998ab7c710fdfbeda73f2a77e4f04f48d875
natureminded <natureminded@users.noreply.github.com>
2017-10-08 16:10:45 -0700

    Timezone stuff added.

--- modules/helpers.js
+++ modules/helpers.js
@@ -2,6 +2,7 @@
 This file is a helper module to our primary socket.io file ('./sockets.js'), and stores all chat data (user and chat text), and assists in some basic chat and user actions.
 */
 module.exports = function(moment){
+	var jstz = require('jstimezonedetect');
 	return {
 		users : [], // Stores our chat users.
 		chatLog : [], // Stores the actual chat log.
@@ -28,12 +29,18 @@ module.exports = function(moment){
 			const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 			const currentTime = moment().tz(timezone).format('hh:mm:ss A');
 			var offset = new Date().getTimezoneOffset();
-			console.log('$$$$$$$$$$$$$');
+
+			const tz = jstz.determine();
+
+			console.log('$$$$$$ TIME STUFF $$$$$$');
 			console.log(timezone);
 			console.log(currentTime);
 			console.log(offset);
 			console.log(moment.tz.guess())
-			console.log('$$$$$$$$$$$$$');
+			console.log('------------------------');
+			console.log(tz.name());
+			console.log('------------------------');
+			console.log('$$$$$$$$$$$$$$$$$$$$$$$$');
 			return currentTime;
 
 		},
